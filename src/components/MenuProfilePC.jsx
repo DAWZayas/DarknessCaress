1996a472080bd6678e1481c71b9b39874c187279
overnaru <juegolin@gmail.com>
2015-11-20 12:50:06 +0100

    gogogogo

--- src/components/MenuProfilePC.jsx
+++ src/components/MenuProfilePC.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { IconMenu, IconButton, FontIcon } from 'material-ui';
 import injecTapEventPlugin from 'react-tap-event-plugin';
 import MenuItem from 'material-ui/lib/menus/menu-item';//the older folder(default) is bug
@@ -14,16 +14,28 @@ export default class MenuProfilePC extends Component {
     super(props);
   }
 
+  handleTouchTap(e){
+    const { navigate } = this.props;
+    const path = e.target.innerHTML.toLowerCase();
+    navigate(path);
+  }
+
   render() {
 
     return (
-      <IconMenu iconButtonElement={<IconButton iconClassName="material-icons">home</IconButton>}>
-       <MenuItem primaryText="Profile" rightIcon={<FontIcon className="material-icons">person</FontIcon>} />
-       <MenuItem primaryText="Settings" rightIcon={<FontIcon className="material-icons">settings</FontIcon>} />
-       <MenuItem primaryText="Help" rightIcon={<FontIcon className="material-icons">help</FontIcon>} />
+      <IconMenu iconButtonElement={<IconButton iconClassName="material-icons">account_circle</IconButton>}>
+       <MenuItem primaryText="Profile" onTouchTap={this.handleTouchTap.bind(this)} leftIcon={<FontIcon className="material-icons">person</FontIcon>} />
+       <MenuItem primaryText="Settings" onTouchTap={this.handleTouchTap.bind(this)} leftIcon={<FontIcon className="material-icons">settings</FontIcon>} />
+       <MenuItem primaryText="Help" onTouchTap={this.handleTouchTap.bind(this)} leftIcon={<FontIcon className="material-icons">help</FontIcon>} />
        <MenuDivider />
-       <MenuItem primaryText="Log out" rightIcon={<FontIcon className="glyphicon glyphicon-log-out" />} />
+       <MenuItem primaryText="Log out" onTouchTap={this.handleTouchTap.bind(this)} leftIcon={<FontIcon className="glyphicon glyphicon-log-out" />} />
       </IconMenu>
     );
   }
 }
+
+MenuProfilePC.propTypes = {
+  // Injected by React Router
+  children: PropTypes.node,
+  navigate: PropTypes.func
+};
