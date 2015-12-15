006d09105b843b714c17cb308a4e9ee17ddc30c5
overnaru <juegolin@gmail.com>
2015-12-15 01:06:38 +0100

    something more

--- src/components/EquipmentSearch.jsx
+++ src/components/EquipmentSearch.jsx
@@ -2,8 +2,8 @@ import React, { Component, PropTypes } from 'react';
 
 export default class EquipmentSearch extends Component{
 	render(){
-		return( <div className="Search">
-            	<input type="text" placeholder="find your weapon" onChange={this.props.searchBy}/>
+		return( <div className="form-group">
+            	<input className="form-control" type="text" placeholder="find your weapon" onChange={this.props.searchBy}/>
            </div>
           ); 
 	}
