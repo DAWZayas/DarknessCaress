e8e226424863410d61ff4c71103de7278b2cbeb1
overnaru <juegolin@gmail.com>
2016-02-16 08:17:00 +0100

    more icons

--- src/components/Profile/AvatarList.jsx
+++ src/components/Profile/AvatarList.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { Avatar } from 'material-ui';
 import IconButton from 'material-ui/lib/icon-button';
 
+import { images } from '../../utils/imageProfileExports';
+
 export default class AvatarList extends Component {
   handleOpenClick(avatar) {
     this.props.changeAvatar(avatar);
@@ -10,16 +12,40 @@ export default class AvatarList extends Component {
     return (
         <div>
           <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar0')} >
-            <Avatar src="http://www.feplanet.net/media/buddyicon/7/bartre.gif" size={70} />
+            <Avatar src{ images.avatar0 } size={70} />
           </IconButton>
           <IconButton onFocus={this.handleOpenClick.bind(this, 'Ouroboros')} >
-            <Avatar src="http://www.feplanet.net/media/buddyicon/7/bartre.gif" size={70} />
+            <Avatar src={ images.avatar1 } size={70} />
           </IconButton>
           <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar2')} >
-            <Avatar src="http://www.feplanet.net/media/buddyicon/7/bartre.gif" size={70} />
+            <Avatar src={ images.avatar2 } size={70} />
           </IconButton>
           <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar3')} >
-            <Avatar src="http://www.feplanet.net/media/buddyicon/7/bartre.gif" size={70} />
+            <Avatar src={ images.avatar3 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar0')} >
+            <Avatar src{ images.avatar4 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'Ouroboros')} >
+            <Avatar src={ images.avatar4 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar2')} >
+            <Avatar src={ images.avatar5 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar3')} >
+            <Avatar src={ images.avatar6 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar0')} >
+            <Avatar src{ images.avatar7 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'Ouroboros')} >
+            <Avatar src={ images.avatar8 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar2')} >
+            <Avatar src={ images.avatar9 } size={70} />
+          </IconButton>
+          <IconButton onFocus={this.handleOpenClick.bind(this, 'avatar3')} >
+            <Avatar src={ images.avatar10 } size={70} />
           </IconButton>
         </div>
     );
--- src/utils/imageProfileExports.js
+++ src/utils/imageProfileExports.js
@@ -1,7 +1,27 @@
 import Ouroboros from '../media/imagesprofile/Ouroboros.png';
-import avatar0 from '../media/imagesprofile/avatar0.png';
+import avatar0 from '../media/imagesprofile/avatar0.jpg';
+import avatar1 from '../media/imagesprofile/avatar1.jpg';
+import avatar2 from '../media/imagesprofile/avatar2.jpg';
+import avatar3 from '../media/imagesprofile/avatar3.jpg';
+import avatar4 from '../media/imagesprofile/avatar4.jpg';
+import avatar5 from '../media/imagesprofile/avatar5.jpg';
+import avatar6 from '../media/imagesprofile/avatar6.jpg';
+import avatar7 from '../media/imagesprofile/avatar7.jpg';
+import avatar8 from '../media/imagesprofile/avatar8.jpg';
+import avatar9 from '../media/imagesprofile/avatar9.jpg';
+import avatar10 from '../media/imagesprofile/avatar10.jpg';
 
 export const images = {
 	Ouroboros: Ouroboros,
-  avatar0: avatar0
+  avatar0: avatar0,
+	avatar1: avatar1,
+	avatar2: avatar2,
+	avatar3: avatar3,
+	avatar4: avatar4,
+	avatar5: avatar5,
+	avatar6: avatar6,
+	avatar7: avatar7,
+	avatar8: avatar8,
+	avatar9: avatar9,
+	avatar10: avatar10,
 };
