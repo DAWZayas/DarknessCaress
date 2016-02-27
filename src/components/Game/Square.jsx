e0a6969359dd723c05a1554a52d221488a61a0e5
CarlosBallesteros <solraksolrak@gmail.com>
2016-02-27 20:30:16 +0100

    added cursor and army icons

--- src/components/Game/Square.jsx
+++ src/components/Game/Square.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 import { images } from '../../utils/imageExports';
 import { heroes } from '../../utils/heroesExports';
+import { cursors } from '../../utils/cursorExports';
 
 export default class Square extends Component {
 
@@ -23,21 +24,45 @@ export default class Square extends Component {
     }
   }
 
+  checkPositions() {
+    const { selectedSquare } = this.props.boardObject.overlayObject;
+    const { movedSquare } = this.props.boardObject.overlayObject;
+    const { position } = this.props;
+    return movedSquare[0] === -1 && movedSquare[1] === -1
+      ? selectedSquare[0] === position[0] && selectedSquare[1] === position[1]
+      : movedSquare[0] === position[0] && movedSquare[1] === position[1];
+  }
+
   render() {
     const { id, image, position, color, unit } = this.props;
+    const isSelected = this.checkPositions();
     return (
       <td className="squareHolder" onClick={() => this.handleSelectSquare(position)}>
         <div className="square">
           <img src={ images[image] } alt={ id } height="48px" width="48px" />
         </div>
         { unit ? (
-        <div className="squareUnit">
-          <img src={ heroes[unit.image] } alt={ id } height="48px" width="48px" />
+        <div>
+          <img className="squareUnit" src={ heroes[unit.image] } alt={ id } height="48px" width="48px" />
+          {
+            this.props.boardObject[unit.army] === this.props.auth.id
+              ? <img className="squareUnit" src={ cursors['good'] } alt={ id } height="48px" width="48px" />
+              : <img className="squareUnit" src={ cursors['evil'] } alt={ id } height="48px" width="48px" />
+          }
         </div>)
         : null }
         <div className="squareOverlay">
           <div className={`overlay${color}`} />
         </div>
+        {
+          isSelected ?
+          (
+            <div className="square">
+              <img className="squareUnit" src={ cursors['cursor'] } alt={ id } height="48px" width="48px" />
+            </div>
+          )
+          : null
+        }
       </td>
     );
   }
