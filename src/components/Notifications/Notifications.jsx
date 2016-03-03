2982683bf21b783c2ab479766b4c2b78e99bb938
CarlosBallesteros <solraksolrak@gmail.com>
2016-03-03 01:08:37 +0100

    preparations for the presentation

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import NotificationManager from './NotificationManager';
+import { Modal, ModalClose } from 'react-modal-bootstrap';
 
 export default class Notifications extends Component {
   componentWillMount() {
@@ -10,7 +11,21 @@ export default class Notifications extends Component {
 
   render(){
     const { notifications, auth } = this.props;
-    return(
+    const admins = [
+      'github:10086900',
+      'github:12071956',
+      'github:10086845',
+      'google:117455282037448467157',
+      'google:116649049236657564771',
+      'google:115675315024314818796'
+    ];
+    return admins.indexOf(this.props.auth.id) === -1
+        ? (
+      <Modal isOpen={true}>
+          <div className="modal-body">
+            <h4>Stop playing with your phone and pay attention to the presentation!!</h4>
+          </div>
+        </Modal>) : (
       <div>
       {
         Object.keys( notifications ).map(( child, index ) => {
